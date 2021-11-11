2ebce5d0803fb21c83805b7c656d7cd3668c974b
Darek Gawęda <realistic115@wp.pl>
2021-11-11 18:42:51 +0100

    [refactor] Query Handler Changes
    - Changed Generic Types for better namings

--- WarehouseManagementSystem/WarehouseManagementSystem.ApplicationServices/API/Handlers/QueryHandler.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem.ApplicationServices/API/Handlers/QueryHandler.cs
@@ -1,15 +1,16 @@
 ﻿using AutoMapper;
 using DataAccess;
 using DataAccess.CQRS.Queries;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using WarehouseManagementSystem.ApplicationServices.API.Domain;
 
 namespace WarehouseManagementSystem.ApplicationServices.API.Handlers
 {
-    public abstract class QueryHandler<TRequest, TResponse, TQuery, TResult, TDomainModel>
-        : IQueryHandler<TRequest, TResponse, TQuery, TResult, TDomainModel>
-        where TQuery : QueryBase<TResult>
-        where TResponse : ResponseBase<TDomainModel>, new()
+    public abstract class QueryHandler<TRequest, TResponse, TQuery, TEntityList, TDomainModelList>
+        : IQueryHandler<TRequest, TResponse, TQuery, TEntityList, TDomainModelList>
+        where TQuery : QueryBase<TEntityList>
+        where TResponse : ResponseBase<TDomainModelList>, new()
     {
         private readonly IMapper _mapper;
         private readonly IQueryExecutor _queryExecutor;
@@ -26,11 +27,11 @@ namespace WarehouseManagementSystem.ApplicationServices.API.Handlers
             var response = CreateResponse(domainModel);
             return response;
         }
-        private async Task<TResult> GetQueryResult(TQuery query) => await _queryExecutor.Execute(query);
+        private async Task<TEntityList> GetQueryResult(TQuery query) => await _queryExecutor.Execute(query);
 
-        private TDomainModel MapDomainModelBy(TResult entityModel) => _mapper.Map<TDomainModel>(entityModel);
+        private TDomainModelList MapDomainModelBy(TEntityList entityModel) => _mapper.Map<TDomainModelList>(entityModel);
 
-        private TResponse CreateResponse(TDomainModel domainModel) => new() { Data = domainModel };
+        private TResponse CreateResponse(TDomainModelList domainModel) => new() { Data = domainModel };
 
         public abstract TQuery CreateQuery(TRequest request);
     }
