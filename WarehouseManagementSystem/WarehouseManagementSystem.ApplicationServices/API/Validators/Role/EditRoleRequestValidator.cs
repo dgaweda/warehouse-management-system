a4507ae1785107b6b14b657213e9bac933d65b5f
Darek Gawęda <realistic115@wp.pl>
2021-11-28 15:30:53 +0100

    Applied Role  Requests Validation

--- WarehouseManagementSystem/WarehouseManagementSystem.ApplicationServices/API/Validators/Role/EditRoleRequestValidator.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem.ApplicationServices/API/Validators/Role/EditRoleRequestValidator.cs
@@ -1,23 +1,26 @@
 ﻿using DataAccess;
 using FluentValidation;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using WarehouseManagementSystem.ApplicationServices.API.Domain.Requests.Role;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace WarehouseManagementSystem.ApplicationServices.API.Validators.Role
 {
-    public class AddRoleRequestValidator : AbstractValidator<AddRoleRequest>
+    public class EditRoleRequestValidator : AbstractValidator<EditRoleRequestValidator>
     {
         private readonly WMSDatabaseContext _context;
-        public AddRoleRequestValidator(WMSDatabaseContext context)
+        public EditRoleRequestValidator(WMSDatabaseContext context)
         {
             _context = context;
 
             RuleFor(x => x.Name).NotEmpty().WithMessage("Role name must be filled");
             RuleFor(x => x.Name).Must(BeUnique).WithMessage("Role of that name already exists.");
             RuleFor(x => x.Description).NotEmpty().WithMessage("You need to describe the role.");
-            RuleFor(x => x.Salary).InclusiveBetween(2500, 99999).WithMessage(x => $"Salary must be equal or greater than 2500. You Entered {x.Salary}");   
+            RuleFor(x => x.Salary).InclusiveBetween(2500, 99999).WithMessage(x => $"Salary must be equal or greater than 2500. You Entered {x.Salary}");
         }
-
+        
         private bool BeUnique(string name) => !_context.Roles.Any(x => x.Name == name);
     }
 }
---
+++ WarehouseManagementSystem/WarehouseManagementSystem.ApplicationServices/API/Validators/Role/RemoveRoleRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManagementSystem.ApplicationServices.API.Domain.Requests.Role;
+
+namespace WarehouseManagementSystem.ApplicationServices.API.Validators.Role
+{
+    public class RemoveRoleRequestValidator : AbstractValidator<RemoveRoleRequest>
+    {
+        private readonly WMSDatabaseContext _context;
+        public RemoveRoleRequestValidator(WMSDatabaseContext context)
+        {
+            _context = context;
+            RuleFor(x => x.RoleId).Must(Exist).WithMessage("Selected role doesn't exist.");
+        }
+        
+        private bool Exist(int id) => _context.Roles.Any(x => x.Id == id);
+    }
+}
