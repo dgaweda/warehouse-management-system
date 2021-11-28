5ed394fc036a2ba8b59d1b66e5f68ece6f3556fc
Darek Gawęda <realistic115@wp.pl>
2021-11-28 14:55:09 +0100

    Applied AddRole Request Validation

---
+++ WarehouseManagementSystem/WarehouseManagementSystem.ApplicationServices/API/Validators/Role/AddRoleRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManagementSystem.ApplicationServices.API.Domain.Requests.Role;
+
+namespace WarehouseManagementSystem.ApplicationServices.API.Validators.Role
+{
+    public class AddRoleRequestValidator : AbstractValidator<AddRoleRequest>
+    {
+        private readonly WMSDatabaseContext _context;
+        public AddRoleRequestValidator(WMSDatabaseContext context)
+        {
+            _context = context;
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Role name must be filled");
+            RuleFor(x => x.Name).Must(BeUnique).WithMessage("Role of that name already exists.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("You need to describe the role.");
+            RuleFor(x => x.Salary).InclusiveBetween(2500, 99999).WithMessage(x => $"Salary must be equal or greater than 2500. You Entered {x.Salary}");
+        }
+
+        private bool BeUnique(string name) => _context.Roles.Any(x => x.Name == name)? false : true;
+    }
+}
