d0b5197c23297309198c0c3460d3c2308acb28e4
Darek Gawęda <realistic115@wp.pl>
2021-11-30 19:51:31 +0100

    applied Product requests Validation

---
+++ WarehouseManagementSystem/WarehouseManagementSystem.ApplicationServices/API/Validators/ProductValidators/EditProductRequestValidator.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess;
+using DataAccess.Entities;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManagementSystem.ApplicationServices.API.Domain.Requests.DeliveryProduct;
+
+namespace WarehouseManagementSystem.ApplicationServices.API.Validators.ProductValidators
+{
+    public class EditProductRequestValidator : AbstractValidator<EditProductRequest>
+    {
+        private readonly WMSDatabaseContext _context;
+        private readonly DbSet<Product> products;
+        public EditProductRequestValidator(WMSDatabaseContext context)
+        {
+            _context = context;
+            products = _context.Set<Product>();
+            RuleFor(x => x.Id).Must(CheckIfIdExists).WithMessage("Can't edit product because it doesn't exists.");
+            RuleFor(x => x.Id).GreaterThan(0);
+
+            RuleFor(x => x.Name).Must(CheckIfNameExists).WithMessage(x => $"Can't edit beacuse product of name: {x.Name} already exists.");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("A product name can't be empty.");
+
+            RuleFor(x => x.Barcode).NotEmpty().WithMessage("A product barcode can't be empty.");
+            RuleFor(x => x.Barcode).Must(CheckIfBarcodeIsUnique).WithMessage(x => $"Passed barcode: {x.Barcode} already exists.");
+
+            RuleFor(x => x.ExpirationDate).GreaterThan(DateTime.Now.Date.AddMonths(3)).WithMessage($"A product expiration date cannot be lower than {DateTime.Now.Date.AddMonths(3)}");
+            RuleFor(x => x.ExpirationDate).NotEmpty().WithMessage("A product expiration date must be set.");
+        }
+        private bool CheckIfIdExists(int Id) => products.Any(x => x.Id == Id);
+
+        private bool CheckIfNameExists(string name) => products.Any(x => x.Name == name);
+
+        private bool CheckIfBarcodeIsUnique(string barcode) => products.Any(x => x.Barcode == barcode);
+    }
+}
---
+++ WarehouseManagementSystem/WarehouseManagementSystem.ApplicationServices/API/Validators/ProductValidators/RemoveProductRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManagementSystem.ApplicationServices.API.Domain.Requests.DeliveryProduct;
+
+namespace WarehouseManagementSystem.ApplicationServices.API.Validators.ProductValidators
+{
+    public class RemoveProductRequestValidator : AbstractValidator<RemoveProductRequest>
+    {
+        private readonly WMSDatabaseContext _context;
+
+        public RemoveProductRequestValidator(WMSDatabaseContext context)
+        {
+            _context = context;
+            RuleFor(x => x.Id).Must(CheckIfIdExists).WithMessage(x => $"Product of ID: {x.Id} doesn't exists.");
+        }
+
+        private bool CheckIfIdExists(int Id) => _context.Products.Any(x => x.Id == Id);
+    }
+}
