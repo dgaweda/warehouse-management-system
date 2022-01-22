ca5e02f11d7d50dbbb2311b8d6c5948ce1b63ddc
Darek Gawęda <realistic115@wp.pl>
2022-01-22 21:31:38 +0100

    New Project Set with scss styles to change themes of bootstrap

--- WarehouseManagementSystemFront/WmsAngularApp/karma.conf.js
+++ WarehouseManagementSystemFront/WmsAngularApp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/WarehouseManagementSystemFront'),
+      dir: require('path').join(__dirname, './coverage/WmsAngularApp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
