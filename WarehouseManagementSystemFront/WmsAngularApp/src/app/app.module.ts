a7219e21df44c3175d000a3558fdc0397d14b905
Darek Gawęda <realistic115@wp.pl>
2022-02-01 21:20:29 +0100

    Added sample data to user data + dropdown ico (not working)

--- WarehouseManagementSystemFront/WmsAngularApp/src/app/app.module.ts
+++ WarehouseManagementSystemFront/WmsAngularApp/src/app/app.module.ts
@@ -8,10 +8,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
-import { BreadcrumbComponent } from './main/breadcrumb/breadcrumb.component';
 import { LoginPanelComponent } from './main/login/login-panel.component';
 import { OrderListComponent } from './main/order-list/order-list.component';
 import { UserDetailsComponent } from './main/user-details/user-details.component';
+import { UserOptionsComponent } from './header/user-options/user-options.component';
 
 @NgModule({
   declarations: [		
@@ -19,10 +19,10 @@ import { UserDetailsComponent } from './main/user-details/user-details.component
     HeaderComponent,
     MainComponent,
     FooterComponent,
-    BreadcrumbComponent,
     LoginPanelComponent,
     OrderListComponent,
-    UserDetailsComponent
+    UserDetailsComponent,
+    UserOptionsComponent
    ],
   imports: [
     BrowserModule,
