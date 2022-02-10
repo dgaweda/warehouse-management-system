ae5c84685efdd2d700e746450591d176e17ed2cf
Darek Gawęda <realistic115@wp.pl>
2022-02-10 21:39:15 +0100

    Modyfing services

--- WarehouseManagementSystem/WarehouseManagementSystem/ClientApp/src/service/delivery.service.ts
+++ WarehouseManagementSystem/WarehouseManagementSystem/ClientApp/src/service/delivery.service.ts
@@ -3,6 +3,8 @@ import { environment } from "src/environments/environment";
 import { Injectable } from "@angular/core";
 import { DeliveryApiUrl } from "src/api/apiUrl";
 import { deliveryModel } from "src/models/delivery.model";
+import { AnyCatcher } from "rxjs/internal/AnyCatcher";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -15,7 +17,11 @@ export class deliveryService {
         this.http = httpClient;
     }
 
-    getDelivery(name?: string): deliveryModel {
+    getDelivery(): Observable<deliveryModel> {
         return this.http.get<deliveryModel>(this.apiUrl + DeliveryApiUrl.getDelivery);
+    }
+
+    addDelivery(delivery: deliveryModel): any {
+        return this.http.post(this.apiUrl + DeliveryApiUrl.addDelivery, delivery);
     }
 }
