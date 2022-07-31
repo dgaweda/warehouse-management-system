a93142c43aadc053c2a79ba0b0fc3d9710e8633c
Darek Gawęda <d.gaweda@npc.pl>
2022-07-31 12:58:39 +0200

    Create roles

---
+++ WarehouseManagementSystem/WarehouseManagementSystem.DataAccess/Seed/DummyDelivery.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccess.Entities;
+
+namespace DataAccess.Seed
+{
+    public static class DummyRoles
+    {
+        public static IEnumerable<Role> CreateDummyRoles()
+        {
+            return new[]
+            {
+                new Role()
+                {
+                    Id = new Guid(),
+                    Description = "Collect orders",
+                    Name = "collector",
+                    Rank = 1,
+                    Salary = 3000
+                },
+                new Role()
+                {
+                    Id = new Guid(),
+                    Description = "sending pallets, getting departures, placing pallets, delivery service",
+                    Name = "warehouseman",
+                    Rank = 2,
+                    Salary = 3500
+                },
+                new Role()
+                {
+                    Id = new Guid(),
+                    Description = "Manage other workers",
+                    Name = "manager",
+                    Rank = 3,
+                    Salary = 4000
+                },
+                new Role()
+                {
+                    Id = new Guid(),
+                    Description = "System administrator",
+                    Name = "admin",
+                    Rank = 4,
+                    Salary = 5000
+                }
+            };
+        }
+    }
+}
---
+++ WarehouseManagementSystem/WarehouseManagementSystem.DataAccess/Seed/DummyRoles.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DataAccess.Entities;
+
+namespace DataAccess.DummyData
+{
+    public class DummyRoles
+    {
+        public IEnumerable<Role> CreateDummyRoles()
+        {
+            return new[]
+            {
+                new Role()
+                {
+                    Id = new Guid(),
+                    Description = "Collect orders",
+                    Name = "collector",
+                    Rank = 1,
+                    Salary = 3000
+                },
+                new Role()
+                {
+                    Id = new Guid(),
+                    Description = "sending pallets, getting departures, placing pallets, delivery service",
+                    Name = "warehouseman",
+                    Rank = 2,
+                    Salary = 3500
+                },
+                new Role()
+                {
+                    Id = new Guid(),
+                    Description = "Manage other workers",
+                    Name = "manager",
+                    Rank = 3,
+                    Salary = 4000
+                },
+                new Role()
+                {
+                    Id = new Guid(),
+                    Description = "System administrator",
+                    Name = "admin",
+                    Rank = 4,
+                    Salary = 5000
+                }
+            };
+        }
+    }
+}
