d1aeece60c096f6092977cf398c9f83449b44b3c
Darek Gawęda <d.gaweda@npc.pl>
2022-10-02 15:54:36 +0200

    Add Pallet tests

---
+++ WarehouseManagementSystem/WMS.DataAccess.Tests/Extensions/PalletExtensionTests/TestData/SetStatusTestData.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DataAccess.Entities;
+using DataAccess.Enums;
+
+namespace WMS.DataAccess.Test.Extensions.PalletExtensionTests.TestData
+{
+    public class SetStatusTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new Pallet()
+                {
+                    Order = new Order(),
+                },
+                PalletStatus.OPEN
+            };
+            
+            yield return new object[]
+            {
+                new Pallet()
+                {
+                    Order = new Order(),
+                    OrderId = new Guid(),
+                },
+                PalletStatus.OPEN
+            };
+            
+            yield return new object[]
+            {
+                new Pallet()
+                {
+                    Order = new Order(),
+                    OrderId = new Guid(),
+                    UserId = new Guid(),
+                },
+                PalletStatus.OPEN
+            };
+            
+            yield return new object[]
+            {
+                new Pallet()
+                {
+                    DepartureId = new Guid(),
+                    Departure = new Departure()
+                    {
+                        State = StateType.CLOSED
+                    }
+                },
+                PalletStatus.SENT
+            };
+            
+            yield return new object[]
+            {
+                new Pallet()
+                {
+                    DepartureId = new Guid(),
+                    Departure = new Departure()
+                    {
+                        State = StateType.OPEN
+                    }
+                },
+                PalletStatus.OPEN
+            };
+            
+            yield return new object[]
+            {
+                new Pallet()
+                {
+                    OrderId = new Guid(),
+                    UserId = new Guid(), 
+                    Order = new Order()
+                    {
+                        OrderState = OrderState.READY_FOR_DEPARTURE
+                    }
+                },
+                PalletStatus.READY_FOR_DEPARTURE
+            };
+            
+            yield return new object[]
+            {
+                new Pallet()
+                {
+                    UserId = new Guid(), 
+                    Order = new Order()
+                    {
+                        OrderState = OrderState.READY_FOR_DEPARTURE
+                    }
+                },
+                PalletStatus.OPEN
+            };
+            
+            yield return new object[]
+            {
+                new Pallet()
+                {
+                    OrderId = new Guid(),
+                    UserId = new Guid(), 
+                    Order = new Order()
+                    {
+                        OrderState = OrderState.IN_PROGRESS
+                    }
+                },
+                PalletStatus.DURING_ORDER_PICKING
+            };
+            
+            yield return new object[]
+            {
+                new Pallet()
+                {
+                    InvoiceId = new Guid(),
+                },
+                PalletStatus.READY_TO_BE_UNFOLDED
+            };
+            
+            yield return new object[]
+            {
+                new Pallet()
+                {
+                    OrderId = new Guid(),
+                    UserId = new Guid(),
+                    Order = new Order()
+                    {
+                        OrderState = OrderState.IN_PROGRESS
+                    },
+                    InvoiceId = new Guid(),
+                },
+                PalletStatus.DURING_ORDER_PICKING
+            };
+        }
+
+            IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- WarehouseManagementSystem/WarehouseManagementSystem.DataAccess/Seeders/Data/DummyProduct.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem.DataAccess/Seeders/Data/DummyProduct.cs
@@ -12,30 +12,35 @@ namespace DataAccess.Seeders.Data
             {
                 new Product()
                 {
+                    Id = new Guid("D862FC8A-F852-4844-871B-0446F445EC40"),
                     ExpirationDate = new DateTime(2025, 1, 1),
                     Name = "Apap",
                     Barcode = "111111111"
                 },
                 new Product()
                 {
+                    Id = new Guid("1F3509D3-258F-49B8-887E-FB55DD427AC9"),
                     ExpirationDate = new DateTime(2027, 2, 15),
                     Name = "Ibuprom",
                     Barcode = "2222222222"
                 },
                 new Product()
                 {
+                    Id = new Guid("DCDD6D03-A28B-4A89-8144-3C9FEDFEFFB7"),
                     ExpirationDate = new DateTime(2023, 3, 25),
                     Name = "CDP Chlorina",
                     Barcode = "3333333333"
                 },
                 new Product()
                 {
+                    Id = new Guid("6578B5FA-1021-4385-80F8-2CF8CFB6A971"),
                     ExpirationDate = new DateTime(2026, 4, 18),
                     Name = "Cholinex",
                     Barcode = "4444444444"
                 },
                 new Product()
                 {
+                    Id = new Guid("FCAD9ED3-754E-44BE-A453-15F433040B67"),
                     ExpirationDate = new DateTime(2024, 5, 4),
                     Name = "Ibuprofen",
                     Barcode = "5555555555"
